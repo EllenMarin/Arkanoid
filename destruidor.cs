7b056c23e505ee76449d043004cd04233ec680bf
Ellen Marin <marin_ellen@hotmail.com>
2022-01-24 16:59:03 +0000

    commit inicial

---
+++ destruidor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destruidor : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision) {
+
+        GameObject collider = collision.collider.gameObject;
+        GameObject.Destroy (collider);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        GameObject.Destroy (collider.gameObject);
+    }
+}
---
+++ pedra.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pedra : MonoBehaviour
+{
+    public Vector2 Velocidade;
+    
+    void Start() {
+
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D> ();
+        rigidbody.AddForce(Velocidade);
+
+
+    }
+
+    
+}
